69ce011e77cdcd56b4aa399f5ce8c3e69151be42
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-06 08:12:53 -0700

    selenium test: test_admin_signin

--- walkthru.py
+++ walkthru.py
@@ -2,7 +2,14 @@ import unittest
 from model import connect_to_db, db, Admin, Cohort, Student, Lab, Pair, Keyword, LabKeyword
 from server import app, session
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from test_seed import create_admin
+from time import sleep
+
+
+
+
+
 
 class AddyAdmin(unittest.TestCase):
 
@@ -32,12 +39,28 @@ class AddyAdmin(unittest.TestCase):
 
 
 
-    def test_view_signin_page(self):
+    def test_admin_view_signin_page(self):
+
+        """Addy views the signin page"""
 
         self.browser.get('http://localhost:5000/signin')
         self.assertEqual(self.browser.title, "Kat Signin")
 
 
+    def test_admin_signin(self):
+        """Addy enters her name and password and hits submit button"""
+
+        self.browser.get('http://localhost:5000/signin')
+
+        self.browser.find_element_by_id("admin-radio").click()
+        name = self.browser.find_element_by_id("signin-name")
+        name.send_keys("Addy Gladdy")
+        password = self.browser.find_element_by_id("signin-password")
+        password.send_keys("pw")
+
+        password.submit()
+        self.assertEqual(self.browser.find_element_by_id("cohort1")
+            .get_attribute("innerHTML"), "Boudicca")
 
 
 
